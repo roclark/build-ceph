f5f661cd80e6554726fae39620ed706f78187218
roclark <robert.d.clark@hp.com>
2015-06-16 16:33:30 -0500

    Added pending steps to determine command if git cloned properly.

--- features/step_definitions/process_command_line.rb
+++ features/step_definitions/process_command_line.rb
@@ -3,6 +3,16 @@ require 'aruba'
 require 'aruba/config'
 include RSpec::Matchers
 
-Given(/^([^`]*) exists$/) do |file_name|
-  check_file_presence(Dir.pwd + '/' + file_name, true)
+
+BRANCH_LOG = 'branches.txt'
+
+
+Then(/^the current branch in git should indicate `([^`]*)`$/) do |branch_name|
+  pending # Planning on using `git branch` and checking if 'branch_name' is
+  # listed, but can't figure out how to use it on my tmp_dir
+end
+
+Then(/^the current repo in git should indicate `([^`]*)`$/) do |repo_name|
+  pending # Same as above, but planning on using `git config --get
+  # remote.origin.url`
 end

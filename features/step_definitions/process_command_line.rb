af7cb805c471fcc4a5e3b7b047eb05ce50b1f96a
roclark <robert.d.clark@hp.com>
2015-06-17 10:09:08 -0500

    Changed comments to strings to improve use with cucumber.

--- features/step_definitions/process_command_line.rb
+++ features/step_definitions/process_command_line.rb
@@ -1,9 +1,7 @@
-Then(/^the current branch in git should indicate `([^`]*)`$/) do |branch_name|
-  pending # Planning on using `git branch` and checking if 'branch_name' is
-  # listed, but can't figure out how to use it on my tmp_dir
+Then(/^the current git branch should be '([^']*)'$/) do |branch_name|
+  pending "will use 'git branch' to see if branch_name is listed"
 end
 
-Then(/^the current repo in git should indicate `([^`]*)`$/) do |repo_name|
-  pending # Same as above, but planning on using `git config --get
-  # remote.origin.url`
+Then(/^the repository should be a clone of '([^']*)'$/) do |repo_name|
+  pending "will use 'git config --get remote.origin.url'"
 end

092fc45966771def002a622edef3117849c92f43
roclark <robert.d.clark@hp.com>
2015-06-17 09:07:42 -0500

    Removed include statements in step definition.

--- features/step_definitions/process_command_line.rb
+++ features/step_definitions/process_command_line.rb
@@ -1,12 +1,3 @@
-require 'rspec/expectations'
-require 'aruba'
-require 'aruba/config'
-include RSpec::Matchers
-
-
-BRANCH_LOG = 'branches.txt'
-
-
 Then(/^the current branch in git should indicate `([^`]*)`$/) do |branch_name|
   pending # Planning on using `git branch` and checking if 'branch_name' is
   # listed, but can't figure out how to use it on my tmp_dir

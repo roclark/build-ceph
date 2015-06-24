f33c2ef83f75511f3ed39502eca4d7ad91e5349b
roclark <robert.d.clark@hp.com>
2015-06-24 13:33:14 -0500

    Moved classes to separate files.

---
+++ String.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+#
+# (C) Copyright 2015 Hewlett-Packard Development Company, L.P.
+# All rights reserved
+
+class String
+  def strip_heredoc
+    indent = scan(/^[ \t]*(?=\S)/).min_by(&:size).size || 0
+    gsub(/^[\t]{#{indent}}/,'').chomp
+  end
+end
--- build-ceph-packages.rb
+++ build-ceph-packages.rb
@@ -3,9 +3,12 @@
 # (C) Copyright 2015 Hewlett-Packard Development Company, L.P.
 # All rights reserved
 
-require 'optparse'
-require 'tmpdir'
+require './CliOptions'
+require './Distribution'
 require 'fileutils'
+require 'optparse'
+require './String'
+require 'tmpdir'
 
 
 VERSION = '0.0.1'
@@ -19,164 +22,6 @@ ERROR_USAGE = 4
 ERROR_BUILD = 5
 
 LOG_FILE = Dir.pwd + '/' + File.basename($PROGRAM_NAME, '.*') + '.log'
-
-
-class String
-  def strip_heredoc
-    indent = scan(/^[ \t]*(?=\S)/).min_by(&:size).size || 0
-    gsub(/^[\t]{#{indent}}/,'').chomp
-  end
-end
-
-
-class CliOptions
-  attr_reader :branch, :build_debs, :build_rpms,
-              :keep_tmpdir, :repo, :tmp_dir
-
-  def initialize
-    @repo = 'https://github.com/HP-Scale-out-Storage/ceph.git'
-    @branch = 'master'
-    @no_debs = false
-    @out_dir = ''
-    @package_manager = :yum
-    @tmp_dir = Dir.mktmpdir
-    @keep_tmpdir = false
-    process_cli_arguments
-    create_output_directory
-    determine_package_manager
-    determine_packages_to_build
-  end
-
-  private
-
-  def create_output_directory
-    unless @out_dir.empty? || File.directory?(@out_dir)
-      Dir.mkdir(@out_dir)
-    end
-  end
-
-  def usage
-    puts <<-EOS.strip_heredoc
-    usage: build-ceph-packages [-h|--help] [--version]
-                         [-b|--branch=<branch-name>] [-r|--repo=<repo-url>]
-                         [--no-debs] [-o|--output=<path>]
-                         [-t|--tmpdir=<tmp-dir>] [-k|--keep-tmpdir]
-
-    This script builds the Ceph RPM and .deb packages from the specified branch
-    of the specified git repository. On a Debian system, only the .deb packages
-    will be generated. On a RHEL/CentOS system, both the RPM and .deb packages
-    will be built.
-    EOS
-  end
-
-  def process_cli_arguments
-    OptionParser.new do |option|
-      option.banner = usage
-
-      option.on('-b', '--branch=<branch-name>', <<-EOS.strip_heredoc) do |branch|
-          Use the specified branch of the repository. Default is to use master.
-          EOS
-        @branch = branch
-      end
-
-      option.on('-h', '--help') do
-        puts option
-        exit EXIT_SUCCESS
-      end
-
-      option.on('-k', '--keep-tmpdir') do
-        @keep_tmpdir = true
-      end
-
-      option.on('--no-debs') do
-        @no_debs = true
-      end
-
-      option.on('-o', '--output=<output-file>') do |output|
-        @out_dir = output
-      end
-
-      option.on('-r', '--repo=<repo-url>', <<-EOS.strip_heredoc) do |repo|
-          Use the specified repository. The URL must be one that git would
-          recognize. If not specified,
-          https://github.com/HP-Scale-out-Storage/ceph will be used.
-          EOS
-        @repo = repo
-      end
-
-      option.on('-t', '--tmpdir=<tmp-dir>', <<-EOS.strip_heredoc) do |tmp_dir|
-          Use the specified temporary directory. Default is to use a randomly
-          generated tmp directory.
-          EOS
-        @tmp_dir = tmp_dir
-      end
-
-      option.on('--version') do
-        puts "#{$PROGRAM_NAME}: #{VERSION}"
-        exit EXIT_SUCCESS
-      end
-    end.parse!
-  end
-
-  def determine_package_manager
-    if File.exist?('/etc/yum')
-      @package_manager = :yum
-    else
-      @package_manager = :apt
-    end
-  end
-
-  def determine_packages_to_build
-    if `lsb_release -is`.match(/RHEL|CentOS/)
-      @build_rpms = true
-      @build_debs = !@no_debs
-    else
-      @build_rpms = false
-      @build_debs = true
-    end
-  end
-end
-
-
-class Distribution
-  def initialize
-  end
-
-  def build_packages
-    fail "This method must be implemented."
-  end
-
-  def install_dependencies
-    fail "This method must be implemented."
-  end
-end
-
-
-class RedHat < Distribution
-  def build_packages
-    `rpmbuild -ba ceph.spec &>> #{LOG_FILE}`
-    fail_if_error(ERROR_BUILD, 'Error building RPM package')
-  end
-
-  def install_dependencies
-    %x{sudo yum -y install `cat deps.rpm.txt` &>> #{LOG_FILE}}
-    fail_if_error(ERROR_DEPENDENCY, 'Error installing dependencies')
-  end
-end
-
-
-class Debian < Distribution
-  def build_packages
-    `(sudo apt-get install dpkg-dev && dpkg-checkbuilddeps && dpkg-build) \
-      &>> #{LOG_FILE}`
-    fail_if_error(ERROR_BUILD, 'Error building .deb package')
-  end
-
-  def install_dependencies
-    %x{sudo apt-get -y install `cat deps.deb.txt` &>> #{LOG_FILE}}
-    fail_if_error(ERROR_DEPENDENCY, 'Error installing dependencies')
-  end
-end
 
 
 def delete_dir(tmp_dir)
@@ -200,10 +45,10 @@ def pull_repo(branch, repo, tmp_dir)
     #{repo} \
     #{tmp_dir} \
     &>> #{LOG_FILE}`
-  fatal_if_error(ERROR_GIT, 'Error pulling from git')
+  fail_if_error(ERROR_GIT, 'Error pulling from git')
 end
 
-def fatal_if_error(exit_code, message)
+def fail_if_error(exit_code, message)
   unless $?.success?
     puts "#{message}. Check #{LOG_FILE} for more details."
     exit exit_code
@@ -212,7 +57,7 @@ end
 
 def generate_config
   `(./autogen.sh && ./configure) &>> #{LOG_FILE}`
-  fatal_if_error(ERROR_CONFIG, 'Error setting up the configuration')
+  fail_if_error(ERROR_CONFIG, 'Error setting up the configuration')
 end
 
 

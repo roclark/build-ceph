267ac806f9266ddb913af42444b4dc1f3c207d19
roclark <robert.d.clark@hp.com>
2015-06-24 09:05:41 -0500

    Removed unnecessary given statement and simplified dependencies query.

--- features/step_definitions/package_verification.rb
+++ features/step_definitions/package_verification.rb
@@ -1,10 +1,6 @@
-Given /^I am running on a CentOS or RHEL machine$/ do
-  expect(`lsb_release -is`).to match(/RHEL|CentOS/)
-end
-
 Then /^the dependencies in the list should all be installed$/ do
   IO.foreach("#{RPM_DEP_LIST}") do |line|
-    expect(`rpm -qa | grep -o #{line}`).to match line
+    expect(`rpm --query #{line}`).to match(/#{line}-/)
   end
 end
 

6e9ba59804af8d3865446d49c6a32df466cb53bb
roclark <robert.d.clark@hp.com>
2015-06-25 13:26:02 -0500

    Added list of packages to check for step definition.

--- features/step_definitions/package_verification.rb
+++ features/step_definitions/package_verification.rb
@@ -9,5 +9,21 @@ Then /^a spec file should be created$/ do
 end
 
 Then /^the packages should be in the output directory$/ do
-  check_file_presence("#{RPM_NAME}", true)
+  if `lsb_release -is`.match(/RHEL|CentOS/)
+    %w[ceph-common fuse rbd-fuse devel radosgw resource-agents librados2
+      libradosstriper1 librbd1 libcephfs1 python-ceph rest-bench ceph-test
+      libcephfs_jni1 cephfs-java libs-compat].each do |filename|
+      check_file_presence("#{filename}.rpm", true)
+    end
+  else
+    %w[ceph ceph-dbg ceph-mds ceph-mds-dbg ceph-fuse ceph-fuse-dbg rbd-fuse
+      rbd-fuse-dbg ceph-common ceph-common-dbg ceph-fs-common
+      ceph-fs-common-dbg ceph-resource-agents librados2 librados2-dbg
+      librados-dev libradosstriper1 libradosstriper-dev librbd1 librbd1-dbg
+      librbd-dev libcephfs1 libcephfs1-dbg libcephfs-dev radosgw radosgw-dbg
+      rest-bench rest-bench-dbg ceph-test ceph-test-dbg python-ceph
+      libcephfs-java libcephfs-jni].each do |filename|
+      check_file_presence("#{filename}.deb", true)
+    end
+  end
 end

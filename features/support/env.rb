d6cdeb900cd780f5dcc554a495f5e900f73d311c
roclark <robert.d.clark@hp.com>
2015-06-29 13:55:19 -0500

    Added new text files to constants that list the expected package names.

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,9 +1,13 @@
 require 'aruba/cucumber'
 require 'rspec/expectations'
 
-TMP_DIR = '/tmp/build-ceph-tmp'
-RPM_DEP_LIST = File.join("#{TMP_DIR}", 'deps.rpm.txt')
+
 CEPH_SPEC = 'ceph.spec'
+DEB_PACKAGE_LIST = 'deb_package_list.txt'
+TMP_DIR = '/tmp/build-ceph-tmp'
+RPM_PACKAGE_LIST = 'rpm_package_list.txt'
+RPM_DEP_LIST = File.join("#{TMP_DIR}", 'deps.rpm.txt')
+
 
 Before do
   @dirs = Dir.pwd
